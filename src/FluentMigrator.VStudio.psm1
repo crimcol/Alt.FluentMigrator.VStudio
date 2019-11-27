bf1cdb9666710247c4206d8eb96712981c83d699
Volodymyr Liashenko <Volodymyr.Liashenko.cw@carlyle.com>
2019-11-26 22:20:19 -0500

    Added commands for updating and reverting database

---
+++ src/FluentMigrator.VStudio.psm1
@@ -0,0 +1,122 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+function GetMigrationSettings($projectName)
+{
+	$p = GetProjectProperties $projectName
+
+	$migrationSettingsFullPath = [IO.Path]::Combine($p.FullPath, 'migrationsetttings.json')
+	$migrationSettings = Get-Content -Raw -Path $migrationSettingsFullPath | ConvertFrom-Json
+
+	$migrationSettings | Add-Member -MemberType NoteProperty -Name SettingsFullPath -Value $migrationSettingsFullPath
+
+	return $migrationSettings
+}
+
+function GetProjectProperties($projectName)
+{
+	$p = If (!$projectName) { Get-Project } Else { Get-Project $projectName }
+
+	$fullPath = $p.Properties.Item("FullPath").Value
+	$outputPath = $p.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value
+	$outputFileName = $p.Properties.Item("OutputFileName").Value
+	$outputFullPath = [IO.Path]::Combine($fullPath, $outputPath)
+	$outputFileFullPath = [IO.Path]::Combine($outputFullPath, $outputFileName)
+	$configFilePath = $outputFileFullPath + ".config"
+
+	$properties = @{
+		Name = $p.Name
+		Project = $p
+		FullPath = $fullPath
+		OutputPath = $outputPath
+		OutputFileName = $outputFileName
+		OutputFullPath = $outputFullPath
+		OutputFileFullPath = $outputFileFullPath
+		ConfigFilePath = $configFilePath
+	}
+
+	$o = New-Object psobject -Property $properties
+
+	return $o
+}
+
+function Fluent-UpdateDatabase($projectName)
+{
+	$migrationProject = GetProjectProperties $projectName
+	Fluent-Build $($migrationProject.Project)
+
+	$migrationSettings = GetMigrationSettings $migrationProject.Name
+	$connectionProject = GetProjectProperties $migrationSettings.ConnectionProjectName
+
+	$params = @(
+		"-t:migrate", 
+		"-db $($migrationSettings.DbProvider)",
+		"-configPath ""$($connectionProject.ConfigFilePath)""",
+		"-c ""$($migrationSettings.ConnectionName)""",
+		"-a ""$($migrationProject.OutputFileFullPath)""",
+		"-wd ""$($migrationProject.OutputFullPath)""")
+		#"-verbose ""FALSE""")
+	$command = "$($migrationSettings.FluentMigrationToolPath) $params"
+	
+	Write-Host $command
+	Invoke-Expression -Command $command
+}
+
+function Fluent-RollbackDatabase
+{
+	[CmdletBinding(DefaultParameterSetName = 'MigrationNumber')]
+		param (
+			[parameter(Position = 0,
+				Mandatory = $true)]
+			[string] $MigrationNumber,
+			$projectName)
+
+	$migrationProject = GetProjectProperties $projectName
+	Fluent-Build $($migrationProject.Project)
+
+	$migrationSettings = GetMigrationSettings $migrationProject.Name
+	$connectionProject = GetProjectProperties $migrationSettings.ConnectionProjectName
+
+	$params = @(
+		"-t rollback:toversion", 
+		"-version $MigrationNumber",
+		"-db $($migrationSettings.DbProvider)",
+		"-configPath ""$($connectionProject.ConfigFilePath)""",
+		"-c ""$($migrationSettings.ConnectionName)""",
+		"-a ""$($migrationProject.OutputFileFullPath)""",
+		"-wd ""$($migrationProject.OutputFullPath)""")
+		#"-verbose ""FALSE""")
+	$command = "$($migrationSettings.FluentMigrationToolPath) $params"
+
+	Write-Host $command
+	Invoke-Expression -Command $command
+}
+
+function Fluent-Build
+{
+	[CmdletBinding(DefaultParameterSetName = '$project')]
+	param ([parameter(Position = 0, Mandatory = $true)] $project)
+	
+	$solutionBuild = $DTE.Solution.SolutionBuild
+    $solutionBuild.BuildProject(
+        $solutionBuild.ActiveConfiguration.Name,
+        $project.UniqueName,
+        <# WaitForBuildToFinish #> $true)
+
+    if ($solutionBuild.LastBuildInfo)
+    {
+        throw "The project '$($project.ProjectName)' failed to build."
+    }
+	
+	Write-Output "'$($project.ProjectName)' project build success."
+}
+
+function GetConfigFilePath($projProps)
+{
+	return $projProps.OutputFileFullPath + ".config"
+}
+
+Export-ModuleMember @('Fluent-UpdateDatabase')
+Export-ModuleMember @('Fluent-RollbackDatabase')
+#Export-ModuleMember @('GetMigrationSettings')
+#Export-ModuleMember @('GetProjectProperties')
---
+++ src/init.ps1
@@ -0,0 +1,22 @@
+param($installPath, $toolsPath, $package)
+
+$importedModule = Get-Module 'FluentMigrator.VStudio'
+$moduleToImport = Test-ModuleManifest (Join-Path $PSScriptRoot 'FluentMigrator.VStudio.psd1')
+$import = $true
+if ($importedModule)
+{
+    if ($importedModule.Version -le $moduleToImport.Version)
+    {
+        
+        Remove-Module 'FluentMigrator.VStudio'
+    }
+    else
+    {
+        $import = $false
+    }
+}
+
+if ($import)
+{
+    Import-Module $moduleToImport
+}
